a7d48e0d1db570fd46937ce7290b5ec4da5796aa
Dubinin Roman <dubininroman273@gmail.com>
2012-04-08 18:50:23 +0700

    virtual attr for checkbox in nested fields_for

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,4 +1,7 @@
 class UserRole < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
+  
+  # Virtual attribute for nested fields_for (checkbox extualy) in user form. 
+  attr_accessor :active
 end
